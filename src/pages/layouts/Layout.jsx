6d57fbbcc354c46adb70425add0ac1d6718e8010
ShineHtetAung <shinehtetaung14499@gmail.com>
2023-09-07 11:52:38 +0630

    home_using_api

---
+++ src/pages/layouts/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
+import Navbar from '../../components/Navbar'
+import { SwitchTransition , CSSTransition } from 'react-transition-group'
+import './styles.css'
+
+export default function Layout() {
+  const location = useLocation();
+
+  return (
+    <div>
+      <Navbar/>
+      <SwitchTransition>
+          <CSSTransition timeout={200} classNames='fade' key={location.pathname}>
+            <div className='max-w-6xl mx-auto p-3'><Outlet /></div>
+          </CSSTransition>
+      </SwitchTransition>
+    </div>
+  )
+}
