c385d6a58eca02364b2a99207a05bca4354b9035
ShineHtetAung <shinehtetaung14499@gmail.com>
2023-09-14 13:28:58 +0630

    register_login_design

---
+++ src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className="w-full max-w-lg mt-16 mx-auto">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h1 className='text-2xl font-bold text-primary my-5'>Login Form</h1>
+            <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email" />
+            </div>
+            <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="password" />
+            {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
+            </div>
+            <div className="flex items-center justify-between">
+            <button className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                Login
+            </button>
+            </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+            &copy;2020 Acme Corp. All rights reserved.
+        </p>
+    </div>
+  )
+}
