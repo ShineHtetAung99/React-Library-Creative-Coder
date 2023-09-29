b9900b636e7f8712471029deac0487ac8ef481a8
ShineHtetAung <shinehtetaung14499@gmail.com>
2023-09-29 11:23:18 +0630

    show_notes_list

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import useFirestore from '../hooks/useFirestore';
+import { useParams } from 'react-router-dom';
+import moment from 'moment/moment';
 
 export default function NoteList() {
+
+    let { id } = useParams();
+    let { getCollection } = useFirestore();
+    let { error, data: notes, loading } = getCollection('notes', ['bookUid', '==', id]);
+    
     return (
-        <div className='border-2 shadow-md p-3 my-3'>
-            <div className='flex space-x-3'>
-                <img src="https://assets.bitdegree.org/online-learning-platforms/storage/media/2018/08/what-is-a-web-developer.jpg" className='w-12 h-12 rounded-full' alt="" />
-                <div>
-                    <h3>Shine Htet A</h3>
-                    <div className='text-gray-400'>20.9.2023</div>
+        !! notes.length && (
+            notes.map(note => (
+                <div className='border-2 shadow-md p-3 my-3' key={note.id}>
+                    <div className='flex space-x-3'>
+                        <img src="https://assets.bitdegree.org/online-learning-platforms/storage/media/2018/08/what-is-a-web-developer.jpg" className='w-12 h-12 rounded-full' alt="" />
+                        <div>
+                            <h3>Shine Htet A</h3>
+                            <div className='text-gray-400'>{moment(note?.date?.seconds * 1000).fromNow()}</div>
+                        </div>
+                    </div>
+                    <div className='mt-3'>
+                        {note.body}
+                    </div>
                 </div>
-            </div>
-            <div className='mt-3'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt saepe perspiciatis at? Sequi fugiat culpa voluptas dolor temporibus eveniet iure error magni asperiores praesentium molestias, nisi dolorum dignissimos assumenda? Nulla?
-            </div>
-        </div>
+            ))
+        )
     )
 }
