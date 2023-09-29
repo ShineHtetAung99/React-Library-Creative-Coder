96db5acbfc89a98629c3b6ed937cfbe7de613c81
ShineHtetAung <shinehtetaung14499@gmail.com>
2023-09-29 11:44:39 +0630

    notes_delete

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,22 +2,31 @@ import React from 'react'
 import useFirestore from '../hooks/useFirestore';
 import { useParams } from 'react-router-dom';
 import moment from 'moment/moment';
+import trashIcon from '../assets/trash.svg';
 
 export default function NoteList() {
 
     let { id } = useParams();
-    let { getCollection } = useFirestore();
+    let { getCollection, deleteDocument } = useFirestore();
     let { error, data: notes, loading } = getCollection('notes', ['bookUid', '==', id]);
+    let deleteNote = async (id) => {
+        await deleteDocument('notes', id);
+    }
     
     return (
         !! notes.length && (
             notes.map(note => (
                 <div className='border-2 shadow-md p-3 my-3' key={note.id}>
-                    <div className='flex space-x-3'>
-                        <img src="https://assets.bitdegree.org/online-learning-platforms/storage/media/2018/08/what-is-a-web-developer.jpg" className='w-12 h-12 rounded-full' alt="" />
+                    <div className='flex space-x-3 justify-between'>
                         <div>
-                            <h3>Shine Htet A</h3>
-                            <div className='text-gray-400'>{moment(note?.date?.seconds * 1000).fromNow()}</div>
+                            <img src="https://assets.bitdegree.org/online-learning-platforms/storage/media/2018/08/what-is-a-web-developer.jpg" className='w-12 h-12 rounded-full' alt="" />
+                            <div>
+                                <h3>Shine Htet A</h3>
+                                <div className='text-gray-400'>{moment(note?.date?.seconds * 1000).fromNow()}</div>
+                            </div>
+                        </div>
+                        <div className='cursor-pointer' onClick={() => deleteNote(note.id)}>
+                            <img src={trashIcon} alt="" />
                         </div>
                     </div>
                     <div className='mt-3'>
